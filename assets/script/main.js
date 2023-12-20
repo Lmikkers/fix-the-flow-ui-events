6bb58ae6808b0385645b0379fa13bc3472af4aeb
Larissa <larissa.mikkers@hotmail.com>
2023-12-20 11:17:47 +0100

    Click
    wanneer je op de eerste button klikt veranderd de kleur van de eerste button

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,17 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+
+// One Click > on one click change color of the first button
+let click = document.querySelector('a:nth-of-type(1)') // hiermee selecteer ik de eerste button
+
+click.addEventListener('click', oneClick)
+
+function oneClick() {
+  click.classList.toggle('click')
+}
+// End function one click
+
+
+
