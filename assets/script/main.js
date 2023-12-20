17f475dedc78819c7f0979c1812747358e65d448
Larissa <larissa.mikkers@hotmail.com>
2023-12-20 11:26:39 +0100

    Dubbel click + this
    Ik heb ervoor gezorgd als je op de 2e button dubbel klikt krijgt het een andere achtergrond, zelfde functie als de 1e button. (this) gebruikt

--- assets/script/main.js
+++ assets/script/main.js
@@ -8,15 +8,21 @@ function jumpHandler() {
 }
 
 
-// One Click > on one click change color of the first button
-let click = document.querySelector('a:nth-of-type(1)') // hiermee selecteer ik de eerste button
+// One Click > Wanneer de eerste button 1 keer wordt geklikt veranderd de kleur van de button
+let oneClick = document.querySelector('a:nth-of-type(1)') // hiermee selecteer ik de eerste button
 
-click.addEventListener('click', oneClick)
+oneClick.addEventListener('click', jumpBackground)
 
-function oneClick() {
-  click.classList.toggle('click')
+function jumpBackground() {
+  this.classList.toggle('changeBackground')
 }
 // End function one click
 
 
 
+// Dubbel click > Met dubbel click krijgt de 2e button een andere kleur achtergrond
+let dubbelClick = document.querySelector('a:nth-of-type(2)')
+
+dubbelClick.addEventListener('dblclick', jumpBackground)
+
+// End Dubbel click
